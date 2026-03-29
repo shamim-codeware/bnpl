f29887decb3c9fc811129451fea32cdc3da6d800
shamim-codeware <m.shamim.codeware@gmail.com>
2026-03-29 16:55:45 +0600

    update

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -12,7 +12,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
-        $schedule->command('app:cp')->everyMinute()->withoutOverlapping();
+        $schedule->command('app:cp')->everyThirtyMinutes()->withoutOverlapping();
     }
 
     /**
